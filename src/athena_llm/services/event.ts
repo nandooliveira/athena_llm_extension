6435e12e1bdb5153f17aaa7d99f1b60ea9369e65
Fernando Oliveira <nandooliveira.al@gmail.com>
2023-11-22 02:04:06 -0300

    Fix address
    Signed-off-by: Fernando Oliveira <nandooliveira.al@gmail.com>

--- src/athena_llm/services/event.ts
+++ src/athena_llm/services/event.ts
@@ -10,7 +10,7 @@ interface EventEntity {
 
 export class EventService {
     save(event: EventEntity) {
-        axios.post('http://127.0.0.1:5000/api/v1/events', event)
+        axios.post('http://localhost:8000/api/v1/events', event)
     .then(function (response) {
       // console.log(response);
     })
